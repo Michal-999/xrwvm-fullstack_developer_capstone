36bbd1386f8d9391f2aea8f3cd3676a7289391b2
Michal-999 <janczuk99@gmail.com>
2026-02-23 17:02:32 -0500

    lint fixes

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -9,17 +9,23 @@ class CarModelInline(admin.StackedInline):
     model = CarModel
     extra = 2
 
+
 class CarMakeInline(admin.StackedInline):
     model = CarMake
+
 
 # CarModelAdmin class
 class CarModelAdmin(admin.ModelAdmin):
     inlines = [CarModelInline]
 
+
 # CarMakeAdmin class with CarModelInline
 class CarMakeAdmin(admin.ModelAdmin):
     inlines = [CarModelInline]
 
+
 # Register models here
 admin.site.register(CarMake)
+
+
 admin.site.register(CarModel)
